15bc0554b567efa8b162634a9b0501275a9a4170
Antônio Roberto  Silva <forevertonny@gmail.com>
2010-10-16 19:19:43 -0300

    - delete_token: for delete a single token - find: for find a single token

--- lib/mini_token.rb
+++ lib/mini_token.rb
@@ -10,12 +10,23 @@ class MiniToken < Struct.new(:db, :token)
     def output(size = nil)
       generate { @token_size = size.nil? ? TOKEN_SIZE : size }
     end
-    
+
     def truncate_tokens
       connection
       @db.truncate
     end
-    
+
+    def delete_token(param)
+      connection
+      @db.filter(:token => param).delete
+    end
+
+    def find(param)
+      connection
+      t = @db.first(:token => param)
+      t[:token] unless t.nil?
+    end
+
     def all
       connection
       @db.all
@@ -28,21 +39,21 @@ class MiniToken < Struct.new(:db, :token)
         @token = TOKEN_CHARACTERS.sample(@token_size).join
         exist_token?
       end
-      
+
     protected
-      
+
       def exist_token?
         connection
         @db.first(:token => @token).nil? ? storing : generate unless @token.nil?
       end
-      
+
       def storing
         @token if @db.insert(:token => @token)
       end
-      
+
       def connection
         sequel = Sequel.sqlite(DB_PATH+(TOKEN_ENV=='production' ? '/mini_token.db' : '/mini_token_test.db' ))
-        @db = sequel[:tokens]     
+        @db = sequel[:tokens]
       end
   end
 end
